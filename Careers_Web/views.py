c1cba925025b11815de59d7c7755296df3527503
harsha <harshabegada@gmail.com>
2024-02-08 16:47:58 +0530

    HOME Function created Successfully

--- Careers_Web/views.py
+++ Careers_Web/views.py
@@ -2,7 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,"index.html")
+    def home(request):
+    career_records=Careers_hub.objects.all()
+    feedback_records=Career_feedback.objects.filter(status="Approved")
+    context={'feedback_records':feedback_records,'career_records':career_records}
+    if request.method=="POST":
+        f_name=request.POST.get('f_name')
+        f_email=request.POST.get('f_email')
+        f_rating=int(request.POST.get('f_rating'))
+        feedback=request.POST.get('feedback')
+        obj=Career_feedback(fullname=f_name,email=f_email,rating=f_rating,feedback=feedback)
+        obj.save()
+        messages.info(request,'Feedback inserted successfully')
+        return render(request,'index.html',context)
+
+    return render(request,'index.html',context)
 
 def loginView(request):
     return render(request,"login.html")
