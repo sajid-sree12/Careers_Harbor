bbe3f9aa8b8dd3dbfbac0da70199e1348a3c326c
venkat <ysaisravani389@gmail.com>
2024-02-08 17:07:01 +0530

    Models created Successfully

--- Careers_Web/admin.py
+++ Careers_Web/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Career_feedback
+from .models import Careers_hub
 # Register your models here.
 
-
+admin.site.register(Careers_hub)
 admin.site.register(Career_feedback)
