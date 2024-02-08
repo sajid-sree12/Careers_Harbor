7862417f0171bea656f0a94196998e6d179cc72d
india56 <156318631+india56@users.noreply.github.com>
2024-02-08 22:00:35 +0530

    Small modification in models.py

--- Careers_Web/models.py
+++ Careers_Web/models.py
@@ -4,16 +4,15 @@ from django.db import models
 
 class Careers_hub(models.Model):
     company_name=models.CharField(max_length=50)
-    company_link=models.TextField(max_length=100)
+    company_link=models.TextField(max_length=120)
     posted=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.company_name
 
 class Career_feedback(models.Model):
-    feedback=models.CharField(max_length=100)
+    feedback=models.CharField(max_length=420)
     fullname=models.CharField(max_length=30)
-    email=models.EmailField()
     rating=models.IntegerField()
     status=models.CharField(default="pending",max_length=30)
     def __str__(self):
